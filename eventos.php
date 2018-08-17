5f3889b53b5b5da4b6ccb3c4c1c3e22067002503
Leo Germani <leogermani@hacklab.com.br>
2018-08-17 18:13:34 -0300

    corrige link para eventos e adiciona breadcrumb

--- eventos.php
+++ eventos.php
@@ -5,6 +5,20 @@
 <!-- Get the menu if is create in panel admin -->
 <?php get_template_part( 'template-parts/menuBellowBanner' ); ?>
 
+<?php if (is_single()): ?>
+	
+	<div class="row justify-content-md-center max-large no-gutters">
+		<div class="col-md-11">
+			<nav aria-label="breadcrumb">
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+					<li class="breadcrumb-item active" aria-current="page">Eventos</li>
+				</ol>
+			</nav>
+		</div>
+	</div>
+<?php endif; ?>
+
 <main role="main" class="mt-5 max-large margin-one-column">
     <div class="row">
         <div class="col col-sm mx-sm-auto">
