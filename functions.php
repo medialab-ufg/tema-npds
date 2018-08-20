e0207c39bbaa344b159827aa078346b1c8c59165
Alvino Rodrigues <alvino.digo@gmail.com>
2018-08-20 10:02:24 -0300

    Alteracoes recebidas

--- functions.php
+++ functions.php
@@ -311,3 +311,12 @@ add_action('pre_get_posts', function($query) {
 
 require_once('inc/list-events-shortcode/listevents_shortcode.php');
 require_once('inc/rewrite-rules.php');
+
+if ( function_exists( 'add_theme_support' ) ) { 
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped)
+
+    // additional image sizes
+    // delete the next line if you do not need additional image sizes
+    add_image_size( 'category-thumb', 300, 9999 ); //300 pixels wide (and unlimited height)
+}
