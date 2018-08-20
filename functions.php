018f074bad686e8b0c19472c5570ddc8e935de6a
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-08-20 09:55:32 -0300

    solved style to be called last.

--- functions.php
+++ functions.php
@@ -7,11 +7,11 @@ function theme_enqueue_styles() {
 
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style',
-		get_stylesheet_directory_uri() . '/style.css',
+		get_stylesheet_uri() . '/style.css',
 		array( $parent_style )
 	);
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 99 );
 
 // Criando novos post types
 function new_post_types() {
