d04aa0315185808a6c12da8c523b0431d8464cf7
Leo Germani <leogermani@gmail.com>
2018-08-20 14:49:47 -0300

    renomeando taxonomias #4

--- archive-profissional.php
+++ archive-profissional.php
@@ -5,24 +5,6 @@
 <!-- Get the menu if is create in panel admin -->
 <?php get_template_part( 'template-parts/menuBellowBanner' ); ?>
 
-<?php if ($post->post_parent > 0): ?>
-	
-	<?php $ancestors = get_post_ancestors(get_the_ID()); ?>
-	
-	<div class="row justify-content-center max-large px-4 px-md-0 no-gutters">
-		<div class="col-md-11">
-			<nav aria-label="breadcrumb">
-				<ol class="breadcrumb">
-					<?php foreach ($ancestors as $ancestor): ?>
-						<li class="breadcrumb-item"><a href="<?php echo get_permalink($ancestor); ?>"><?php echo get_the_title($ancestor); ?></a></li>
-					<?php endforeach; ?>
-					<li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
-				</ol>
-			</nav>
-		</div>
-	</div>
-<?php endif; ?>
-
 <main role="main" class="mt-5 max-large px-4 px-md-0">
 <?php
 	if ( have_posts() ) {
@@ -32,9 +14,11 @@
 				<div class="tainacan-title">
 					<h2 class="title-1">Banco de profissionais</h2>
 				</div>
+				<!--
 				<div class="header-meta text-muted post-details tainacan-details">
 					Atualizado em <?php tainacan_post_date(); ?>.
 				</div>
+				-->
 			</div>
 		</div>
 
@@ -57,9 +41,9 @@
 											<?php } ?>
 										</div>
 										<div class="lista-profissionais__texto">
-											<span class="lista-profissionais__categoria"><?php the_terms( $post->ID, 'type-area', '', ', ', ' ' );?></span>
+											<span class="lista-profissionais__categoria"><?php the_terms( $post->ID, 'area_profissional', '', ', ', ' ' );?></span>
 											<strong class="lista-profissionais__nome"><?php the_title(); ?></strong>
-											<span class="lista-profissionais__dado"><?php the_terms( $post->ID, 'type_especialidade', '<b>Especialidades:</b> ', ', ', ' ' );?></span>
+											<span class="lista-profissionais__dado"><?php the_terms( $post->ID, 'especialidade_profissional', '<b>Especialidades:</b> ', ', ', ' ' );?></span>
 											<span class="lista-profissionais__dado"><b>Cidade:</b> <?php echo get_post_meta($post->ID, 'profissionais_cidade', true); ?> - <?php echo get_post_meta($post->ID, 'profissionais_estado', true); ?></span>
 											<span class="lista-profissionais__dado"><b>Telefone:</b> <?php echo get_post_meta($post->ID, 'profissionais_telefone', true); ?></span>
 											<span class="lista-profissionais__dado"><b>E-mail:</b> <?php echo get_post_meta($post->ID, 'profissionais_email', true); ?></span>
