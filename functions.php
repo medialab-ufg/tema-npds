3d1c5299e301714519c7d869b03b0bf839848668
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-08-20 10:01:44 -0300

    Return the function to display the stylesheet of child theme.

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function theme_enqueue_styles() {
 
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style',
-		get_stylesheet_uri() . '/style.css',
+	get_stylesheet_directory_uri() . '/style.css',
 		array( $parent_style )
 	);
 }
