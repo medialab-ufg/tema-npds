50e1b647c51746b002c9dc620a6ef6eb0aec8691
Alvino Rodrigues <alvino.digo@gmail.com>
2018-12-13 11:39:13 -0200

    Correcao para o menu de navegacao ser exibido corretamente

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <!-- Get the banner to display -->
 <?php get_template_part( 'template-parts/bannerHeader' ); ?>
 <!-- Get the menu if is create in panel admin -->
-<?php get_template_part( 'template-parts/menuBellowBanner' ); ?>
+<?php get_template_part( 'template-parts/menubellowbanner' ); ?>
 
 <div class="container-fluid mt-5 max-large">
 	<div class="row">
